be5b9120fe4b736b6673d734fd15b0b92be10bcf
Chris Mellor <cmellor@gmail.com>
2025-03-29 17:13:44 +0700

    feat: add some opinionated Laravel defaults

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,13 @@
 
 namespace App\Providers;
 
+use Carbon\CarbonImmutable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Client\RequestException;
+use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Validation\Rules\Password;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +25,69 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->applySensiblePasswordDefaults();
+        $this->configureExceptions();
+        $this->configureModels();
+        $this->ensureDatesAreImmutable();
+        $this->prohibitDestructiveCommandsFromRunning();
+    }
+
+    /**
+     * @return void
+     */
+    private function applySensiblePasswordDefaults(): void
+    {
+        Password::defaults(
+            fn() => $this->app->isProduction()
+                ? $this->passwordDefaults()
+                : null
+        );
+    }
+
+    /**
+     * @return Password
+     */
+    private function passwordDefaults(): Password
+    {
+        return Password::min(size: 8)
+            // ->letters()
+            // ->mixedCase()
+            // ->numbers()
+            // ->symbols()
+            ->uncompromised();
+    }
+
+    /**
+     * @return void
+     */
+    private function configureExceptions(): void
+    {
+        RequestException::dontTruncate();
+    }
+
+    /**
+     * @return void
+     */
+    protected function configureModels(): void
+    {
+        Model::shouldBeStrict(!app()->isProduction());
+
+        Model::unguard();
+    }
+
+    /**
+     * @return void
+     */
+    protected function ensureDatesAreImmutable(): void
+    {
+        Date::use(handler: CarbonImmutable::class);
+    }
+
+    /**
+     * @return void
+     */
+    private function prohibitDestructiveCommandsFromRunning(): void
+    {
+        DB::prohibitDestructiveCommands(app()->isProduction());
     }
 }
